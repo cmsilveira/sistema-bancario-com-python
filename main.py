1c82ec749cd392a59f9860a41bbba79c0a4fec7f
Carlos Matheus <carlos.matheus@outlook.com>
2023-04-21 15:34:50 -0300

    Criado main.py
    - implementada estrutura inicial do aplicativo com um menu
    - implementada prováveis variáveis necessárias
    - implementada opção SAIR

---
+++ main.py
@@ -0,0 +1,29 @@
+menu = '''
+Selecione uma das opções abaixo:
+--------------------------------
+  [1] depósito
+  [2] saque
+  [3] extrato
+  ---
+  [0] Sair
+=> '''
+
+saldo_atual = 0.0
+limite_por_saque = 500.00
+extrato = ""
+quantidade_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    match opcao:
+        # opção SAIR
+        case '0':
+            print("Aplicativo será encerrado.", end = " ")
+            confirmar = input("Confirma? S/N: ")
+            if confirmar.upper() == 'S':
+                break
+        case other:
+            print("Opção inválida")
+
